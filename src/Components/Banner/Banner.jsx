683ebf2464f2ed0cd7bd805c7a79942822a2f0a0
Mishu78 <meherun2002@gmail.com>
2026-03-30 12:39:53 +0600

    banner added

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -1,45 +1,26 @@
+import { TbCurrentLocation } from "react-icons/tb";
 import React from 'react';
 
 const Banner = () => {
     return (
-        <div className='min-h-[60vh] container justify-items-center mx-auto'>
-        
-            <div className="navbar bg-base-100 shadow-sm">
-  <div className="navbar-start">
-    <div className="dropdown">
-      <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
-      </div>
-      <ul
-        tabIndex="-1"
-        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-        <li><a>Products</a></li>
-        <li><a>Features</a></li>
-        <li><a>Pricing</a></li>
-        <li><a>Testimonials</a></li>
-        <li><a>FAQ</a></li>
-      </ul>
-    </div>
-    <a className="font-bold text-4xl bg-linear-to-r from-[#4F39F6] to-[#9514FA] bg-clip-text text-transparent ">DigiTools</a>
-  </div>
-  <div className="navbar-center hidden lg:flex">
-    <ul className="menu menu-horizontal px-1">
-      <li><a>Products</a></li>
-        <li><a>Features</a></li>
-        <li><a>Pricing</a></li>
-        <li><a>Testimonials</a></li>
-        <li><a>FAQ</a></li>
-    </ul>
-  </div>
-  <div className="navbar-end">
-    <div className='flex items-center gap-4'>
-        <img src="/src/assets/products/shopping-cart.png" alt="" />
-        <h3>Login</h3>
-         <a className="btn rounded-3xl bg-linear-to-r from-[#4F39F6] to-[#9514FA] text-white">Get Started</a>
-    </div>
-   
-  </div>
+        <div className="flex gap-5 container justify-center mx-auto items-center">
+            <div>
+            <button className='mb-4 flex items-center gap-2 bg-[#dae4ee] rounded-4xl p-4 '><TbCurrentLocation className="rounded-full text-white bg-linear-to-r from-[#4F39F6] to-[#9514FA]"/><h2 className=' bg-linear-to-r from-[#4F39F6] to-[#9514FA] text-transparent bg-clip-text'>New: AI-Powered Tools Available</h2></button>
+
+            <h1 className="mb-5 text-5xl font-bold">Supercharge Your <br></br> Digital Workflow</h1>
+            <p className="text-gray-500">Access premium AI tools, design assets, templates, and productivity <br></br> software—all in one place. Start creating faster today. <br></br> Explore Products
+Watch Demo</p>
+<div className="flex gap-4 mt-5">
+
+<button className='btn-primary  bg-linear-to-r from-[#4F39F6] to-[#9514FA] rounded-4xl p-4 text-white'>Explore Products</button>
+<button className=' rounded-4xl p-4 bg-linear-to-r from-[#4F39F6] to-[#9514FA] text-transparent bg-clip-text border-2 border-purple-600 flex items-center gap-2 '><img src="/src/assets/Play.png" alt="" /> Watch demo</button>
+
 </div>
+
+            </div>
+            <div>
+                <img className='w-125 h-147.5' src="/src/assets/banner.png" alt="" />
+            </div>
         </div>
     );
 };
