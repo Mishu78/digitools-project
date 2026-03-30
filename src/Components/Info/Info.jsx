2e8248c521b659828945669109b3b40b7edfc0b6
Mishu78 <meherun2002@gmail.com>
2026-03-30 14:25:10 +0600

    updated info section

--- src/Components/Info/Info.jsx
+++ src/Components/Info/Info.jsx
@@ -2,12 +2,11 @@ import React from 'react';
 
 const Info = () => {
     return (
-        <div className='bg-linear-to-r from-[#4F39F6] to-[#9514FA] text-white flex justify-center'>
-           <div className='mr-10 text-center'>
+        <div className='bg-linear-to-r from-[#4F39F6] to-[#9514FA] text-white flex justify-center p-6 mb-5'>
+           <div className='mr-10 text-center '>
             <h1 className='text-5xl font-bold'>50K+</h1>
             <p>Active Users</p>
             </div> 
-            <div className='w-[2px] h-6'></div>
            <div className='mr-10 text-center'>
             <h1 className='text-5xl font-bold'>200+</h1>
             <p>Premium Tools</p>
