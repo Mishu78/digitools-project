ec2705d3cef52d41daa0b0a10ba8113661be9276
Mishu78 <meherun2002@gmail.com>
2026-03-31 12:15:30 +0600

    remove button added

--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -1,12 +1,24 @@
 import React from 'react';
 
-const Cart = ({carts}) => {
+const Cart = ({carts,setCarts}) => {
     
-    const totalPrice = carts.reduce((sum,item)=>sum+item.price,0)
+    const totalPrice = carts.reduce((sum,item)=>sum+item.price,0);
+    const handlePayment=()=>{
+        setCarts([]);
+    }
+    const handleRemove = (item)=>{
+         const filteredArray=carts.filter(cart=>cart.id !== item.id)
+         setCarts(filteredArray);
+    }
     return (
         <div className='container mx-auto'>
             <h2 className='text-2xl font-bold mb-5'>Your cart</h2>
-          {
+
+            {
+                carts.length===0 ? <p className='text-5xl text-gray-500 font-bold mt-40 text-center mb-40'>Cart is empty</p> : 
+                <>
+                 <div>
+                    {
         carts.map(item=><div className='mb-10' key={item.id}>
             <div className='flex justify-between'>
             <div className='flex gap-4'>
@@ -22,7 +34,7 @@ const Cart = ({carts}) => {
             </div>
 
 <div className=''>
-    <button>Remove</button>
+    <button onClick={()=>handleRemove(item)} className='text-red-500'>Remove</button>
 </div>
 </div>
         </div>)
@@ -31,6 +43,16 @@ const Cart = ({carts}) => {
             <div>Total</div>
             <div>${totalPrice}</div>
         </div>
+        <div className='mb-10'>
+            <button onClick={handlePayment} className='btn-primary  bg-linear-to-r from-[#4F39F6] to-[#9514FA] rounded-4xl p-3 text-white w-full'>Proceed To Checkout</button>
+        </div>
+            </div>
+      
+                
+                </>
+            }
+           
+
         </div>
     );
 };
--- src/Components/Info/Info.jsx
+++ src/Components/Info/Info.jsx
@@ -5,8 +5,8 @@ const Info = () => {
     
     return (
         <div>
-        <div className='bg-linear-to-r from-[#4F39F6] to-[#9514FA] text-white flex justify-center p-6  mt-40'>
-           <div className='mr-10 text-center '>
+        <div className='bg-linear-to-r from-[#4F39F6] to-[#9514FA] text-white flex justify-around p-6  mt-40'>
+           <div className='mr-10 text-center pl-5 '>
             <h1 className='text-5xl font-bold'>50K+</h1>
             <p>Active Users</p>
             </div> 
@@ -14,10 +14,16 @@ const Info = () => {
             <h1 className='text-5xl font-bold'>200+</h1>
             <p>Premium Tools</p>
             </div> 
-           <div className='mr-10 text-center'>
+           <div className='mr-10 text-center pl-5'>
             <h1 className='text-5xl font-bold'>4.9</h1>
             <p>Rating</p>
             </div> 
+        </div>
+        <div className='justify-center text-center mt-40'>
+            <h1 className='text-5xl font-bold mb-3'>Premium Digital Tools</h1>
+            <p className='text-gray-500 mb-8'>Premium Digital Tools
+Choose from our curated collection of premium digital products designed <br></br>
+to boost your productivity and creativity.</p>
         </div>
 </div>
     );
